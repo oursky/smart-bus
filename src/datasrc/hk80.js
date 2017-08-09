7858d2a44523e933710f6b2167d5115af3aba6da
hidden-dimensions <hidden-dimensions@users.noreply.github.com>
2017-08-09 14:25:07 +0800

    eta get all bounds of a route

--- src/datasrc/hk80.js
+++ src/datasrc/hk80.js
@@ -5,11 +5,14 @@
  * http://www.geodetic.gov.hk/transform/tformAPI_manual.pdf
  */ 
 module.exports = (function(){
+// Imports
+// -----------------------------------------------------------------
 const http = require('http');
 
+// -----------------------------------------------------------------
 class HK80Convertor {
-	constructor() {
-	}
+    constructor() {
+    }
     /**
      * Get Lat,Lon from HK80 Coordinate
      * @public
@@ -18,23 +21,23 @@ class HK80Convertor {
      * @param {!function(number,number)} cb void callback(lat,lon)
      * @return {undefined} No return value
      */
-	get_latlon(x,y,cb) {
-		// TODO: handle error
-	    return http.get({
-    	    hostname: 'www.geodetic.gov.hk',
-        	path: '/transform/tformAPI.aspx?ver=1&hke='+x+'&hkn='+y
-    	}, (response) => {
-        	var body = '';
-        	response.on('data', (d) => { body += d;} );
-	        response.on('end', () => {
-	        	var latlon = body.split(","); 
-				cb(latlon[0],latlon[1]);
-        	});
-	    });
-	}
+    get_latlon(x,y,cb) {
+        // TODO: handle error
+        return http.get({
+                hostname: 'www.geodetic.gov.hk',
+                path: '/transform/tformAPI.aspx?ver=1&hke='+x+'&hkn='+y
+            }, (response) => {
+                var body = '';
+                response.on('data', (d) => { body += d;} );
+                response.on('end', () => {
+                    var latlon = body.split(","); 
+                    cb(latlon[0],latlon[1]);
+                });
+            });
+    }
 }
 
 // EXPORTS
-// ------------------------------------------------
+// -----------------------------------------------------------------
 return HK80Convertor;
 }());   
--- src/datasrc/kmb.js
+++ src/datasrc/kmb.js
@@ -4,11 +4,14 @@
  * https://http://search.kmb.hk/KMBWebSite/
  */
 module.exports = (function(){
+// Imports
+// -----------------------------------------------------------------
 const http = require('http');
 
-class KMBGrabber {
-	constructor() {
-	}
+// -----------------------------------------------------------------
+class KMBDataSource {
+    constructor() {
+    }
     /**
      * Get bounds of a route
      * @public
@@ -17,24 +20,24 @@ class KMBGrabber {
      * @return {undefined} No return value
      */
      get_bounds(route, cb) {
-	    return http.get({
-    	    hostname: 'search.kmb.hk',
-        	path: '/KMBWebSite/Function/FunctionRequest.ashx?action=getroutebound&route=' + route
-    	}, (response) => {
-        	var body = '';
-        	response.on('data', (d) => { body += d;} );
-	        response.on('end', () => {
-	        	var json = JSON.parse(body);
-	        	var list = [];
-	        	if (json['result']) {
-					for (var item of json['data']) {
-						list.push(parseInt(item['BOUND']));
-					}
-				}
-				cb(list);
-        	});
-	    });
-	}
+        return http.get({
+                hostname: 'search.kmb.hk',
+                path: '/KMBWebSite/Function/FunctionRequest.ashx?action=getroutebound&route=' + route
+            }, (response) => {
+                var body = '';
+                response.on('data', (d) => { body += d;} );
+                response.on('end', () => {
+                    var json = JSON.parse(body);
+                    var list = [];
+                    if (json['result']) {
+                        for (var item of json['data']) {
+                            list.push(parseInt(item['BOUND']));
+                        }
+                    }
+                    cb(list);
+                });
+            });
+    }
     /**
      * Get bus-stops of a route
      * @public
@@ -43,49 +46,49 @@ class KMBGrabber {
      * @param {!function(Object,Array<Object>)} cb void callback(basic,stops)
      * @return {undefined} No return value
      */
-	get_stops(route, bound, cb) {
-	    return http.get({
-    	    hostname: 'search.kmb.hk',
-        	path: '/KMBWebSite/Function/FunctionRequest.ashx?action=getstops&route=' + route + '&bound=' + bound + '&serviceType=1'
-    	}, (response) => {
-        	var body = '';
-        	response.on('data', (d) => { body += d;} );
-	        response.on('end', () => {
-	        	var json = JSON.parse(body);
-	        	var jbasic = json['data']['basicInfo'];
-	        	var basic = {
-	        		origin_en : jbasic['OriEName'],
-	        		origin_tc : jbasic['OriCName'],
-	        		origin_sc : jbasic['OriSCName'],
-	        		dest_en   : jbasic['DestEName'],
-	        		dest_tc   : jbasic['DestCName'],
-	        		dest_sc   : jbasic['DestSCName'],
-	        		racecourse: (jbasic['Racecourse']=='Y'),
-	        		special   : (jbasic['Special']=='Y'),
-	        		overnight : (jbasic['Overnight']=='Y'),
-	        		airport   : (jbasic['Airport']=='Y'),
-	        	};
-	        	var stops = [];
-	        	for (var stop of json['data']['routeStops']) {
-	        		stops.push({
-	        			route: stop['Route'],
-	        			bound: stop['Bound'],
-	        			seq  : stop['Seq'],
-	        			bsicode: stop['BSICode'],
-	        			name_en: stop['EName'],
-	        			name_tc: stop['CName'],
-	        			name_sc: stop['SCName'],
-	        			location_en: stop['ELocation'],
-	        			location_tc: stop['CLocation'],
-	        			location_sc: stop['SCLocation'],
-	        			x          : stop['X'],
-	        			y          : stop['Y'],
-	        			fare       : stop['AirFare'],
-	        		});
-	        	}
-	        	cb(basic, stops);
-        	});
-	    });
+    get_stops(route, bound, cb) {
+        return http.get({
+                hostname: 'search.kmb.hk',
+                path: '/KMBWebSite/Function/FunctionRequest.ashx?action=getstops&route=' + route + '&bound=' + bound + '&serviceType=1'
+            }, (response) => {
+                var body = '';
+                response.on('data', (d) => { body += d;} );
+                response.on('end', () => {
+                    var json = JSON.parse(body);
+                    var jbasic = json['data']['basicInfo'];
+                    var basic = {
+                        origin_en : jbasic['OriEName'],
+                        origin_tc : jbasic['OriCName'],
+                        origin_sc : jbasic['OriSCName'],
+                        dest_en   : jbasic['DestEName'],
+                        dest_tc   : jbasic['DestCName'],
+                        dest_sc   : jbasic['DestSCName'],
+                        racecourse: (jbasic['Racecourse']=='Y'),
+                        special   : (jbasic['Special']=='Y'),
+                        overnight : (jbasic['Overnight']=='Y'),
+                        airport   : (jbasic['Airport']=='Y'),
+                    };
+                    var stops = [];
+                    for (var stop of json['data']['routeStops']) {
+                        stops.push({
+                            route: stop['Route'],
+                            bound: stop['Bound'],
+                            seq  : stop['Seq'],
+                            bsicode: stop['BSICode'],
+                            name_en: stop['EName'],
+                            name_tc: stop['CName'],
+                            name_sc: stop['SCName'],
+                            location_en: stop['ELocation'],
+                            location_tc: stop['CLocation'],
+                            location_sc: stop['SCLocation'],
+                            x          : stop['X'],
+                            y          : stop['Y'],
+                            fare       : stop['AirFare'],
+                        });
+                    }
+                    cb(basic, stops);
+                });
+        });
 	}
     /**
      * Get arrival estimation of a route for a specific bus stop
@@ -97,50 +100,50 @@ class KMBGrabber {
      * @param {!function(Array<string>)} cb void callback(timeStrings)
      * @return {undefined} No return value
      */
-	get_eta(route, bound, seq, bsicode, cb) {
-		bsicode = bsicode.replace(/-/g, '');	// chop - characters
+    get_eta(route, bound, seq, bsicode, cb) {
+        bsicode = bsicode.replace(/-/g, '');	// chop - characters
 
-		var now = new Date();
-		var utc = now.getTime();
-		var ms = utc % 100;
-		var timestamp = now.toISOString().replace(/T/, ' ').replace(/\..+/, '')+'.'+ms+'.';
+        var now = new Date();
+        var utc = now.getTime();
+        var ms = utc % 100;
+        var timestamp = now.toISOString().replace(/T/, ' ').replace(/\..+/, '')+'.'+ms+'.';
 		
-		var sep = "--31" + timestamp + "13--";
-		var token = route + sep + bound + sep + "1" + sep + bsicode + sep + seq + sep + utc;
-		var token64 = "EA" + new Buffer(token).toString('base64');
-		var postData = "token="+encodeURIComponent(token64)+"&t="+encodeURIComponent(timestamp);
+        var sep = "--31" + timestamp + "13--";
+        var token = route + sep + bound + sep + "1" + sep + bsicode + sep + seq + sep + utc;
+        var token64 = "EA" + new Buffer(token).toString('base64');
+        var postData = "token="+encodeURIComponent(token64)+"&t="+encodeURIComponent(timestamp);
 		
-		var options = {
-    	    hostname: 'search.kmb.hk',
-        	path: '/KMBWebSite/Function/FunctionRequest.ashx/?action=get_ETA&lang=1',
-		    method: 'POST',
-    		headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
-					'Content-Length': Buffer.byteLength(postData)
-			},
-			form: {
-				'token': token64,
-				't': timestamp
-			}
-		};
-	    var req = http.request(options, (response) => {
-        	var body = '';
-        	response.on('data', (d) => { body += d;} );
-	        response.on('end', () => {
-	        	var json = JSON.parse(body);
-	        	var list = [];
-       			for (var item of json['data']['response']) {
-					list.push(item['ex']);
-				}
-	        	cb(list);
-        	});
-	    });
-	    req.write(postData);
-		req.end();
-	}
+        var options = {
+            hostname: 'search.kmb.hk',
+            path: '/KMBWebSite/Function/FunctionRequest.ashx/?action=get_ETA&lang=1',
+            method: 'POST',
+            headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(postData)
+            },
+            form: {
+                'token': token64,
+                't': timestamp
+            }
+        };
+        var req = http.request(options, (response) => {
+            var body = '';
+            response.on('data', (d) => { body += d;} );
+            response.on('end', () => {
+                var json = JSON.parse(body);
+                var list = [];
+                for (var item of json['data']['response']) {
+                    list.push(item['ex']);
+                }
+                cb(list);
+            });
+        });
+        req.write(postData);
+        req.end();
+    }
 }
 
 // EXPORTS
-// ------------------------------------------------
-return KMBGrabber;
+// -----------------------------------------------------------------
+return KMBDataSource;
 }());   
